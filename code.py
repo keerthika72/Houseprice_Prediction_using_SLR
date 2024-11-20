92f11c52e1c140269619f67f85aee96a8e2346bc
keerthika72 <112416700+keerthika72@users.noreply.github.com>
2024-11-20 10:59:52 +0530

    Update code.py

--- code.py
+++ code.py
@@ -10,16 +10,14 @@ import pickle
 #Loading the dataset:
 dataset = pd.read_csv(r"C:\Users\monik\Downloads\SLR - House price prediction\House_data.csv")
 
-#Fetching the specified data from the dataset.
+
 space = dataset["sqft_living"]
 price=dataset["price"]
-# Defining the independent and dependent varibale:
 x = np.array(space).reshape(-1,1) # x is the independent variable.
 y = np.array(price) # y is the dependent varibale{price}
 
 
 # Now splitting the dataset for the training and the testing:
-# Lets split the training and the testing dataset in the ratio of 75% and 25%.
 x_train , x_test , y_train , y_test = train_test_split(x,y,test_size=0.25,random_state=0) #Random state = o , so that it won't take the vaue randomly from the dataset.
 
 x_train = x_train.reshape(-1,1)
@@ -57,11 +55,11 @@ y_890 = regressor.predict([[890]])
 print(y_560)
 print(y_890)
 
-# Check model performance ,for the better performance the bias and variance should be same.
-bias = regressor.score(x_train, y_train) # calcuating the bias value score for the model.{Train score}
+
+bias = regressor.score(x_train, y_train)
 variance = regressor.score(x_test, y_test) #{test score} variance score for thr model.
-train_mse = mean_squared_error(y_train, regressor.predict(x_train)) # calculating the mean square error for the training dataset.
-test_mse = mean_squared_error(y_test, y_pred) # Calculating the mean square error for the testing dataset.
+train_mse = mean_squared_error(y_train, regressor.predict(x_train)) 
+test_mse = mean_squared_error(y_test, y_pred) 
 
 print(f"Training Score (R^2): {bias:.2f}")
 print(f"Testing Score (R^2): {variance:.2f}")
@@ -75,4 +73,4 @@ with open(filename, 'wb') as file:
 print("Model has been pickled and saved as house price.pkl")
 
 import os 
-print(os.getcwd()) #this will show the file location where the data is saved
+print(os.getcwd())
